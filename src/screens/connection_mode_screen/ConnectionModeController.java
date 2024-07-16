0caa51e7a219d25eed9b033d2da8bd17c2bb2499
Mahmoud Darwish <mahmoudsaaddarwish1@gmail.com>
2024-07-16 18:04:35 +0200

    Separate ConnectionMode UI screen on another folder

---
+++ src/screens/connection_mode_screen/ConnectionModeController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package screens.connection_mode_screen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+import tictactoegame.components.XOButton;
+import utils.constants.AppConstants;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mahmoud
+ */
+public class ConnectionModeController implements Initializable {
+
+      @FXML
+    private VBox buttonContainer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+        // Create the custom buttons and add them to the VBox
+        XOButton vsComputerBtn = new XOButton("Offline", () -> handleOfflineButtonAction(), AppConstants.xIconPath );
+        XOButton vsFriendBtn = new XOButton("Online", () -> handleOnlineButtonAction(), AppConstants.oIconPath);
+        
+        buttonContainer.getChildren().addAll(vsComputerBtn, vsFriendBtn);
+    }
+    
+    private void handleOfflineButtonAction() {
+        System.out.println("Navigate to Connection mode screen");
+    }
+    
+    private void handleOnlineButtonAction() {
+        System.out.println("Navigate to Login");
+    }
+    
+}
--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -19,7 +19,7 @@ public class TicTacToeGame extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/screens/connection_mode_screen/ConnectionModeScreen.fxml"));
         
         Scene scene = new Scene(root);
         
