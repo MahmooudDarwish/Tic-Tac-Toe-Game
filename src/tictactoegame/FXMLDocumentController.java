3a92438f3b7be5b057d3c9c5c238431fd9462c47
Mahmoud Darwish <mahmoudsaaddarwish1@gmail.com>
2024-07-16 17:49:53 +0200

    Add X&O Icon Assets Add XOButton reusable component across the application Add abstract class to contain all constants across the application Implement ConnectionMode UI screen

--- src/tictactoegame/FXMLDocumentController.java
+++ src/tictactoegame/FXMLDocumentController.java
@@ -1,35 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoegame;
 
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import tictactoegame.components.XOButton;
+import utils.constants.AppConstants;
+
 
 /**
- *
- * @author Mahmoud
+ * FXML Controller class.
  */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private VBox buttonContainer;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     
+        // Create the custom buttons and add them to the VBox
+        XOButton vsComputerBtn = new XOButton("Vs Computer", () -> handleVsComputerAction(), AppConstants.xIconPath );
+        XOButton vsFriendBtn = new XOButton("Vs Friend", () -> handleVsFriendAction(), AppConstants.oIconPath);
+        
+        buttonContainer.getChildren().addAll(vsComputerBtn, vsFriendBtn);
+    }
+    
+    private void handleVsComputerAction() {
+        System.out.println("Show Vs Computer alert dialog");
+    }
+    
+    private void handleVsFriendAction() {
+        System.out.println("Show Vs Friend alert dialog");
+    }
 }
---
+++ src/utils/constants/AppConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils.constants;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public abstract class AppConstants {
+    
+     public final static String xIconPath = "/assets/icons/XIcon.png";
+     public final static String oIconPath = "/assets/icons/OIcon.png";
+
+    
+}
