d785d9e0e054d429e9b6fba1c3111cbae017a957
Omar Gohary <omar.gohary123@gmail.com>
2024-07-17 19:32:50 +0200

    adding players dialog screen

--- src/screens/game_mode_screen/GameModeUiController.java
+++ src/screens/game_mode_screen/GameModeUiController.java
@@ -9,7 +9,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Pair;
 import tictactoegame.components.XOButton;
 import utils.constants.AppConstants;
 
@@ -27,10 +35,60 @@ public class GameModeUiController implements Initializable {
     }    
     private void handlePlayPCButtonAction() {
         System.out.println("Navigate to players bob up");
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Dialog");
+        dialog.setHeaderText("Please enter your names");
+
+         
+        ButtonType loginButtonType = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        // Create the username and password labels and fields
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField player1 = new TextField();
+        player1.setPromptText("player 1");
+        
+
+        grid.add(new Label("player 1:"), 0, 0);
+        grid.add(player1, 1, 0);
+       
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.show();
     }
     
     private void handlePlayFriendButtonAction() {
-        System.out.println("Navigate to players bob up");
+        
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Dialog");
+        dialog.setHeaderText("Please enter your names");
+
+         
+        ButtonType loginButtonType = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        // Create the username and password labels and fields
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField player1 = new TextField();
+        player1.setPromptText("player 1");
+        TextField palyer2 = new TextField();
+        palyer2.setPromptText("player 2");
+
+        grid.add(new Label("player 1:"), 0, 0);
+        grid.add(player1, 1, 0);
+        grid.add(new Label("player 2"), 0, 1);
+        grid.add(palyer2, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.show();
     }
     private void handleBackButtonAction() {
         System.out.println("back to connection mode screen");
