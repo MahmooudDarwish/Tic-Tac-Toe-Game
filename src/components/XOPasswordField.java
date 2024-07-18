0d43025db493f005d79b420fd4032bd12696054f
Mohamed Abdelrehim Abdelhady Aly <moabdelraheem19@gmail.com>
2024-07-18 12:44:28 +0300

    add usability code for login and its UI

---
+++ src/components/XOPasswordField.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoegame.components;
+
+import javafx.scene.control.PasswordField;
+
+/**
+ *
+ * @author Mohammed
+ */
+public class XOPasswordField extends PasswordField {
+    
+    public  XOPasswordField(String promptText){
+     setPromptText(promptText);
+     setStyle("-fx-background-color: white;"
+                + " -fx-border-color: grey;"
+                + " -fx-border-width: 2px;"
+                + " -fx-border-radius: 10px; "
+                + "-fx-background-radius: 10px; "
+                + " -fx-font-size: 16px;"
+                + " -fx-text-fill: black;"
+        ); // Button style with rounded corners
+    }
+
+}
